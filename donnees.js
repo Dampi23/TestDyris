76b39e0f73efd7a472e228ffd96f1f4e160e21fc
Dampi23 <102037489+Dampi23@users.noreply.github.com>
2026-05-03 10:09:55 +0200

    Update QCM link and add new question options

--- donnees.js
+++ donnees.js
@@ -1885,7 +1885,9 @@ faux("Il faut diviser la tension par la vitesse : 60 / 1000.");
 // ====================================================================================
 theme("St laurent : QCM appareillage (1 Question : Lien vers un QCM extérieur");
 
-https://electrotoile.eu/qcm/qcm_appareillage/ 
+quest("APP00 : Lancer le QCM electrotoile en ligne//a");
+rep("[x] Démarrer le test");
+url_quitter("https://electrotoile.eu/qcm/qcm_appareillage/");
 
 // ====================================================================================
 // SECTION : L'électronique
